15ffefcc4fa37aad377c3333797de7a510c2ae6f
161250213 <674527347@qq.com>
2019-05-12 18:29:58 +0800

    Initial commit

---
+++ src/main/java/semoviegroup/semovie/SeMovieApplication.java
@@ -0,0 +1,13 @@
+package semoviegroup.semovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeMovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/semoviegroup/semovie/SeMovieApplicationTests.java
@@ -0,0 +1,16 @@
+package semoviegroup.semovie;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SeMovieApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
